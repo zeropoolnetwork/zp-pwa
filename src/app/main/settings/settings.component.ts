0d1ce29098f0efdd96f658723ce09a3a87b2d9c7
artemvorobev <artall64@gmail.com>
2020-03-01 17:12:45 +0200

    Delete account

--- src/app/main/settings/settings.component.ts
+++ src/app/main/settings/settings.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
+  confirmed = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  resetAccount() {
+    // TODO: clear local storag
+  }
 }
