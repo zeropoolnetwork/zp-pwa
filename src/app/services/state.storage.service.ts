7108362b4f56316ea048cef26fe8cb8d3f3971da
artemvorobev <artall64@gmail.com>
2020-03-01 23:20:46 +0200

    Added indexed DB support

--- src/app/services/state.storage.service.ts
+++ src/app/services/state.storage.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HistoryState, MyUtxoState } from 'zeropool-lib';
 import { StorageMap } from '@ngx-pwa/local-storage';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+export type MyUtxoStateHex = MyUtxoState<string>;
+export type MyUtxoStateInt = MyUtxoState<bigint>;
 
 @Injectable()
 export class YourService {
-  constructor(private storage: StorageMap) {}
+  constructor(private storage: StorageMap) {
+  }
 }
 
 @Injectable({
@@ -12,38 +18,51 @@ export class YourService {
 })
 export class StateStorageService {
 
-  constructor() {
+  constructor(private storage: StorageMap) {
   }
 
-  get historyState(): HistoryState | undefined {
-    return this.getItem<HistoryState>('history-state');
+  getHistoryState(): Observable<HistoryState> {
+    return this.storage.get<HistoryState>('history-state').pipe(
+      // tap(() => {
+      //   debugger
+      // })
+    ) as Observable<HistoryState>;
   }
 
-  set historyState(val: HistoryState) {
-    this.setItem('history-state', val);
+  saveHistory(val: HistoryState): void {
+    this.storage.set('history-state', val)
+      .subscribe(() => {
+      });
   }
 
-  get utxoState(): MyUtxoState<string> | undefined {
-    return this.getItem<MyUtxoState<string>>('utxo-state');
+  getUtxoState(): Observable<MyUtxoStateHex> {
+    const result$ = this.storage.get<MyUtxoStateHex>('utxo-state').pipe(
+      tap(() => {
+        debugger
+      })
+    );
+    return result$ as Observable<MyUtxoStateHex>;
   }
 
-  set utxoState(val: MyUtxoState<string>) {
-    this.setItem('utxo-state', val);
+  saveUtxo(val: MyUtxoStateHex): void {
+    this.storage.set('utxo-state', val)
+      .subscribe(() => {
+      });
   }
 
 
-  private setItem(filed: string, item: any): void {
-    localStorage.setItem(filed, JSON.stringify(item));
-  }
-
-  private getItem<T>(field: string): T {
-    const data = localStorage.getItem(field);
-
-    try {
-      return JSON.parse(data);
-    } catch (e) {
-      localStorage.removeItem(field);
-      return undefined;
-    }
-  }
+  // private setItem(filed: string, item: any): void {
+  //   localStorage.setItem(filed, JSON.stringify(item));
+  // }
+  //
+  // private getItem<T>(field: string): T {
+  //   const data = localStorage.getItem(field);
+  //
+  //   try {
+  //     return JSON.parse(data);
+  //   } catch (e) {
+  //     localStorage.removeItem(field);
+  //     return undefined;
+  //   }
+  // }
 }
