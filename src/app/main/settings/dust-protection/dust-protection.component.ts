e9eafc0b7b932fe2b467155325f98fb68ec4e81f
Kirill <krboktv@gmail.com>
2020-03-04 00:21:05 +0300

    add dust form

--- src/app/main/settings/dust-protection/dust-protection.component.ts
+++ src/app/main/settings/dust-protection/dust-protection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-dust-protection',
@@ -7,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DustProtectionComponent implements OnInit {
 
-  constructor() { }
+  public protectionForm: FormGroup = this.fb.group({
+    maxGasFee: [''],
+    minUtxoSize: [''],
+  });
+
+  get maxGasFee(): number {
+    return this.protectionForm.get('maxGasFee').value;
+  }
+
+  get minUtxoSize(): number {
+    return this.protectionForm.get('minUtxoSize').value;
+  }
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+  }
 
   ngOnInit(): void {
   }
