22744f5db356526011417ded92f30d699af3bc0a
Kirill <krboktv@gmail.com>
2020-03-04 11:42:25 +0300

    edit config

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -5,7 +5,7 @@ export const environment = {
   sideChainAddress: '0x4CA54A3FaF2A98534Ad950433bc7bC0bEfB4d82a',
   sideChainRpc: 'https://kovan.infura.io/v3/a3f4d001c1fc4a359ea70dd27fd9cb51',
   // relayerUrl: 'http://127.0.0.1:3000',
-  relayerUrl: 'https://relayer.zeropool.network',
+  relayerUrl: 'https://testnet.relayer.zeropool.network',
   ethToken: '0x0000000000000000000000000000000000000000',
   relayerFee: 320 * (10 ** 9)
 };
