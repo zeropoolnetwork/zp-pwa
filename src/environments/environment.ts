f69f59168e4fb070d481a5a7c1785f00a9843370
Kirill <krboktv@gmail.com>
2020-03-04 17:14:21 +0300

    update config + lib version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,11 @@
 export const environment = {
   production: false,
   chainId: '0x4',
-  contractAddress: '0x9fC4d8178C31d4EecdF184aB51853bB88cabb859',
-  sideChainAddress: '0x4CA54A3FaF2A98534Ad950433bc7bC0bEfB4d82a',
+  contractAddress: '0x08734d27E29f82C31CC6C141f0b879E45D7398d0',
+  sideChainAddress: '0xD938D8E635a35aD032cE019E5Ca95812f0555679',
   sideChainRpc: 'https://kovan.infura.io/v3/a3f4d001c1fc4a359ea70dd27fd9cb51',
-  // relayerUrl: 'http://127.0.0.1:3000',
-  relayerUrl: 'https://testnet.relayer.zeropool.network',
+  relayerUrl: 'http://127.0.0.1:3000',
+  // relayerUrl: 'https://testnet.relayer.zeropool.network',
   ethToken: '0x0000000000000000000000000000000000000000',
   relayerFee: 320 * (10 ** 9)
 };
