f4ed5fc8cb9b9e9539f674a13e2bc68d521f2d8f
artemvorobev <artall64@gmail.com>
2020-03-06 18:40:38 +0200

    added dev environment

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  chainId: '0x4',
+  contractAddress: '0x08734d27E29f82C31CC6C141f0b879E45D7398d0',
+  sideChainAddress: '0xD938D8E635a35aD032cE019E5Ca95812f0555679',
+  sideChainRpc: 'https://kovan.infura.io/v3/a3f4d001c1fc4a359ea70dd27fd9cb51',
+  // relayerUrl: 'http://127.0.0.1:3000',
+  relayerUrl: 'https://dev.relayer.zeropool.network',
+  ethToken: '0x0000000000000000000000000000000000000000',
+  relayerFee: 320 * (10 ** 9)
+};
