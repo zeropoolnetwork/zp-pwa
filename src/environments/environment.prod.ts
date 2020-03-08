665eb7dbedebecf4b1ca71cf7004a66737e52a7d
Kirill <krboktv@gmail.com>
2020-03-08 04:00:20 +0300

    edit side chains contracts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   chainId: '0x1',
   contractAddress: '0x20306be1fbb05064956514aa687f6ab1bee54e7d',
-  sideChainAddress: '0xb8a370e061BF78B155CFc613FF4cd7055A6d1224',
+  sideChainAddress: '0x581698a84C17C82e3aFa1ced008a3098598e4CC1',
   sideChainRpc: 'https://kovan.infura.io/v3/a3f4d001c1fc4a359ea70dd27fd9cb51',
   // relayerUrl: 'http://127.0.0.1:3000',
   relayerUrl: 'https://relayer.zeropool.network',
